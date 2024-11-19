06d4782031fb374fe2fc4b9e9f2a8b2fdc7dd5aa
Nikhil <nikhiltamboli17@gmail.com>
2024-11-19 17:50:24 +0530

    autoreloads every day

--- script.js
+++ script.js
@@ -50,6 +50,9 @@ async function fetchSheetData() {
 
 function updateTime() {
     let d = new Date();
+    if(d.getSeconds()==0 && d.getHours()==0 && d.getMinutes()==0){
+      window.location.reload();
+    }
     var time = `${d.getHours()>9?d.getHours():"0"+d.getHours()} : ${d.getMinutes()>9?d.getMinutes():"0"+d.getMinutes()} : ${d.getSeconds()>9?d.getSeconds():"0"+d.getSeconds()}`
     document.getElementById("timeString").innerHTML= time
     setTimeout(updateTime,1000);
